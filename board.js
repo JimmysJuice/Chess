f6e2b9823ba9300e9e83b028043d52bd548e7f48
JimmysJuice <52631394+JimmysJuice@users.noreply.github.com>
2021-03-07 23:45:22 -0500

    added move validity checker for bishop, rook and queen

--- board.js
+++ board.js
@@ -7,6 +7,7 @@ class Board
         this.width = tileSize * 8;
         this.isHoldingPiece = false;
         this.heldPiece = {};
+        this.heldPieceValidMoves = [];
         this.isWhitesTurn = true;
 
         this.setUpPieces();
@@ -27,10 +28,10 @@ class Board
         this.whitePieces.push(new Pawn(1, 1, true));
         this.whitePieces.push(new Pawn(2, 1, true));
         this.whitePieces.push(new Pawn(3, 1, true));
-        this.whitePieces.push(new Pawn(4, 1, true));
+        this.whitePieces.push(new Pawn(4, 2, true));
         this.whitePieces.push(new Pawn(5, 1, true));
         this.whitePieces.push(new Pawn(6, 1, true));
-        this.whitePieces.push(new Pawn(7, 1, true));
+        this.whitePieces.push(new Pawn(6, 2, true));
 
         this.blackPieces.push(new Rook(0, 7, false));
         this.blackPieces.push(new Knight(1, 7, false));
@@ -45,10 +46,10 @@ class Board
         this.blackPieces.push(new Pawn(1, 6, false));
         this.blackPieces.push(new Pawn(2, 6, false));
         this.blackPieces.push(new Pawn(3, 6, false));
-        this.blackPieces.push(new Pawn(4, 6, false));
+        this.blackPieces.push(new Pawn(4, 5, false));
         this.blackPieces.push(new Pawn(5, 6, false));
         this.blackPieces.push(new Pawn(6, 6, false));
-        this.blackPieces.push(new Pawn(7, 6, false));
+        this.blackPieces.push(new Pawn(6, 5, false));
     }
 
     show()
@@ -102,6 +103,37 @@ class Board
         return square;
     }
 
+    idSquareByPos(x, y)
+    {
+        let square = {};
+        square.isEmpty = true;
+
+        square.x = x;
+        square.y = y;
+        
+        for (let i = 0; i < this.whitePieces.length; i++)
+        {
+            if (this.whitePieces[i].boardPosition.x === square.x && this.whitePieces[i].boardPosition.y === square.y)
+            {
+                square.piece = this.whitePieces[i];
+                square.isEmpty = false;
+                return square;
+            }
+        }
+        
+        for (let i = 0; i < this.blackPieces.length; i++)
+        {
+            if (this.blackPieces[i].boardPosition.x === square.x && this.blackPieces[i].boardPosition.y === square.y)
+            {
+                square.piece = this.blackPieces[i];
+                square.isEmpty = false;
+                return square;
+            }
+        }
+
+        return square;
+    }
+
     isOffBoard(x, y)
     {
         if (x < 0 || x > 7 || y < 0 || y > 7)
@@ -111,16 +143,228 @@ class Board
         return false;
     }
 
-    isLegalMove(square)
+    getValidDiagonalMoves(piece)
     {
-        if (!square.isEmpty)
+        let validMoves = [];
+        let hitPiece = false;
+        let x = piece.boardPosition.x;
+        let y = piece.boardPosition.y;
+        let square;
+        // check  up and to the left
+        while(!hitPiece && x > 0 && y < 7)
         {
-            if (this.heldPiece.isWhite === square.piece.isWhite)
+            x--; y++;
+            square = this.idSquareByPos(x, y);
+
+            if (square.isEmpty)
             {
-                return false;
+                validMoves.push({x: x, y: y});
+            }
+            else if (square.piece.isWhite != this.heldPiece.isWhite)
+            {
+                validMoves.push({x: x, y: y});
+                hitPiece = true;
+            }
+            else
+            {
+                hitPiece = true;
             }
         }
-        return true;
+
+        x = piece.boardPosition.x;
+        y = piece.boardPosition.y;
+        hitPiece = false;
+
+        // check up and to the right
+        while(!hitPiece && x < 7 && y < 7)
+        {
+            x++; y++;
+            square = this.idSquareByPos(x, y);
+
+            if (square.isEmpty)
+            {
+                validMoves.push({x: x, y: y});
+            }
+            else if (square.piece.isWhite != this.heldPiece.isWhite)
+            {
+                validMoves.push({x: x, y: y});
+                hitPiece = true;
+            }
+            else
+            {
+                hitPiece = true;
+            }
+        }
+
+        x = piece.boardPosition.x;
+        y = piece.boardPosition.y;
+        hitPiece = false;
+
+        // check down and to the left
+        while(!hitPiece && x > 0 && y > 0)
+        {
+            x--; y--;
+            square = this.idSquareByPos(x, y);
+
+            if (square.isEmpty)
+            {
+                validMoves.push({x: x, y: y});
+            }
+            else if (square.piece.isWhite != this.heldPiece.isWhite)
+            {
+                validMoves.push({x: x, y: y});
+                hitPiece = true;
+            }
+            else
+            {
+                hitPiece = true;
+            }
+        }
+
+        x = piece.boardPosition.x;
+        y = piece.boardPosition.y;
+        hitPiece = false;
+
+        // check down and to the right
+        while(!hitPiece && x < 7 && y > 0)
+        {
+            x++; y--;
+            square = this.idSquareByPos(x, y);
+
+            if (square.isEmpty)
+            {
+                validMoves.push({x: x, y: y});
+            }
+            else if (square.piece.isWhite != this.heldPiece.isWhite)
+            {
+                validMoves.push({x: x, y: y});
+                hitPiece = true;
+            }
+            else
+            {
+                hitPiece = true;
+            }
+        }
+
+        return validMoves;
+    }
+
+    getValidVerticalAndHorizontalMoves(piece)
+    {
+        let validMoves = [];
+        let hitPiece = false;
+        let x = piece.boardPosition.x;
+        let y = piece.boardPosition.y;
+        let square;
+
+        // check up
+        while(!hitPiece && y < 7)
+        {
+            y++;
+            square = this.idSquareByPos(x, y);
+
+            if (square.isEmpty)
+            {
+                validMoves.push({x: x, y: y});
+            }
+            else if (square.piece.isWhite != this.heldPiece.isWhite)
+            {
+                validMoves.push({x: x, y: y});
+                hitPiece = true;
+            }
+            else
+            {
+                hitPiece = true;
+            }
+        }
+
+        x = piece.boardPosition.x;
+        y = piece.boardPosition.y;
+        hitPiece = false;
+
+        // check down
+        while(!hitPiece && y > 0)
+        {
+            y--;
+            square = this.idSquareByPos(x, y);
+
+            if (square.isEmpty)
+            {
+                validMoves.push({x: x, y: y});
+            }
+            else if (square.piece.isWhite != this.heldPiece.isWhite)
+            {
+                validMoves.push({x: x, y: y});
+                hitPiece = true;
+            }
+            else
+            {
+                hitPiece = true;
+            }
+        }
+
+        x = piece.boardPosition.x;
+        y = piece.boardPosition.y;
+        hitPiece = false;
+
+        // check left
+        while(!hitPiece && x > 0)
+        {
+            x--;
+            square = this.idSquareByPos(x, y);
+
+            if (square.isEmpty)
+            {
+                validMoves.push({x: x, y: y});
+            }
+            else if (square.piece.isWhite != this.heldPiece.isWhite)
+            {
+                validMoves.push({x: x, y: y});
+                hitPiece = true;
+            }
+            else
+            {
+                hitPiece = true;
+            }
+        }
+
+        x = piece.boardPosition.x;
+        y = piece.boardPosition.y;
+        hitPiece = false;
+
+        // check right
+        while(!hitPiece && x < 7)
+        {
+            x++;
+            square = this.idSquareByPos(x, y);
+
+            if (square.isEmpty)
+            {
+                validMoves.push({x: x, y: y});
+            }
+            else if (square.piece.isWhite != this.heldPiece.isWhite)
+            {
+                validMoves.push({x: x, y: y});
+                hitPiece = true;
+            }
+            else
+            {
+                hitPiece = true;
+            }
+        }
+        return validMoves;
+    }
+
+    isLegalMove(square)
+    {
+        for (let i = 0; i < this.heldPieceValidMoves.length; i++)
+        {
+            if (square.x === this.heldPieceValidMoves[i].x && square.y === this.heldPieceValidMoves[i].y)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     pickUpPiece(x, y)
@@ -129,9 +373,23 @@ class Board
 
         if (!square.isEmpty && square.piece.isWhite === this.isWhitesTurn)
         {
-            this.heldPiece = square.piece;
             square.piece.isPickedUp = true;
             board.isHoldingPiece = true;
+            this.heldPiece = square.piece;
+
+            switch(this.heldPiece.name)
+            {
+                case "bishop":
+                    this.heldPieceValidMoves = this.getValidDiagonalMoves(this.heldPiece);
+                    break;
+                case "rook":
+                    this.heldPieceValidMoves = this.getValidVerticalAndHorizontalMoves(this.heldPiece);
+                    break;
+                case "queen":
+                    this.heldPieceValidMoves = this.getValidDiagonalMoves(this.heldPiece);
+                    let temp = this.getValidVerticalAndHorizontalMoves(this.heldPiece);
+                    temp.forEach(move => this.heldPieceValidMoves.push(move));
+            }
         }
     }
 
